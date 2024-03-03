dbc8408cd14dc9d48c9ad1fea29853e288b8cc24
Aditya Sista <aditya16791@gmail.com>
2024-03-03 16:13:31 +0530

    LC20 - Valid Parentheses

---
+++ LeetCodeSolutions/EasyLC/LC20.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLC
+{
+    /// <summary>
+    /// Valid parenthesis.
+    /// </summary>
+    public class LC20
+    {
+        public bool IsValid(string s)
+        {
+            var parDict = new Dictionary<char, char>() { { ')', '(' }, { ']', '[' }, { '}', '{' } };
+            var parStack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{') parStack.Push(c);
+                else
+                {
+                    if (parStack.Count == 0 || parDict[c] != parStack.Pop()) return false;
+                }
+            }
+            return parStack.Count == 0;
+        }
+    }
+}
