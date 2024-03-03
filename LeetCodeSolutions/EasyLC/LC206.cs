8e480e29b673c3c6f22b73345401888ab161497e
Aditya Sista <aditya16791@gmail.com>
2024-03-03 12:16:05 +0530

    LC206 - Reverse a linked list: Iterative and Recursive.

---
+++ LeetCodeSolutions/EasyLC/LC206.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLC
+{
+    public class LC206
+    {
+        // Iterative solution
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode curr = head;
+            ListNode prev = null;
+            ListNode next = null;
+
+            while (curr != null)
+            {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
+            return head;
+        }
+
+        // Recursive solution
+        //public ListNode ReverseList(ListNode head)
+        //{
+        //    if (head == null || head.next == null) return head;
+
+        //    var newHead = ReverseList(head.next);
+        //    var curr = head.next;
+        //    curr.next = head;
+        //    head.next = null;
+        //    return newHead;
+        //}
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null) 
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
